57ea4bd74158568b7a3b999501eaa802d0c98ebf
Joseph Sugamele <jsugamele@gmail.com>
2022-03-21 12:34:11 -0400

    updates

---
+++ common/scripts/profile_update_check.sh
@@ -0,0 +1,46 @@
+# shellcheck shell=bash
+
+cd "$DOTFILESLOC" &> /dev/null || exit
+
+git remote update
+
+STATUS=$(git status -sb)
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+
+if [[ "$STATUS" == *"behindd"* ]]; then
+    echo "Looks like profile is out of date!"
+    REPLY=$(bash -c 'read -e -p "Do you want to update the profile? [y/N]" tmp; echo $tmp')
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        git pull
+    fi
+fi
+
+
+if [ "$LOCAL" = "$REMOTE" ]; then
+    echo "Up-to-date"
+elif [ "$LOCAL" = "$BASE" ]; then
+    echo "Need to pull"
+    echo "Looks like profile is out of date!"
+    REPLY=$(bash -c 'read -e -p "Do you want to update the profile? [y/N]" tmp; echo $tmp')
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+        echo "no pull"
+    then
+        echo "pull"
+        git pull
+    fi
+elif [ "$REMOTE" = "$BASE" ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
+
+#mv ~/.dotfiles/.dotfiles_location /home/.dotfiles_location
+#mv /home/.dotfiles_location ./
+
+cd ~ || exit
